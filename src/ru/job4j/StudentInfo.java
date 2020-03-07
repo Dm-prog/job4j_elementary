9d8e9c2b66bb79592f7aadc9c428d8b82fb66720
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-07 17:21:43 +0400

    3.3. Разбор первой программы.[#250950]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dmitriy Kotov");
+        System.out.println("26.02.80");
+    }
+}
