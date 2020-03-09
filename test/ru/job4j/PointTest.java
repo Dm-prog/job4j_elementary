da732b43aa783a7917b1b6a0c6955fae9ce45869
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-09 18:42:39 +0400

    3. Тесты для определения расстояния между точками.[#250987]

---
+++ test/ru/job4j/PointTest.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.condition.Point;
+
+public class PointTest {
+    @Test
+    public void distance() {
+        int x1In = 0;
+        int y1In = 0;
+        int x2In = 2;
+        int y2In = 0;
+        double expected = 2.0;
+        double out = Point.distance(x1In, y1In, x2In, y2In);
+        Assert.assertEquals(expected, out, 0.1);
+    }
+}
