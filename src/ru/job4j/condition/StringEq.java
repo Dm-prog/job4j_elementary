56f2466cf7d5e754639ef39f9482d334ffb47760
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-10 16:46:33 +0400

    4.1.0. Операторы сравнения в String.[#250901]

--- src/ru/job4j/condition/StringEq.java
+++ src/ru/job4j/condition/StringEq.java
@@ -3,6 +3,7 @@ package ru.job4j.condition;
 public class StringEq {
     public static boolean check(String login) {
         String root = new String("root");
+        login = new String("root");
         boolean access = login.equals(root);
         return access;
     }
