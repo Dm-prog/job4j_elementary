8312d9006ba15e0c43eba32dd5bbea5a7955b749
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-11 14:29:47 +0400

    4.2. Тернарное сравнение[#250897]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,14 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left;
         boolean max = true;
-        if (left > right) {
-            return max ? right : left;
-        }
-        if (right > left) {
-            return max ? right : left;
-        }
-        return result;
+        return max ? right : left;
     }
 }
