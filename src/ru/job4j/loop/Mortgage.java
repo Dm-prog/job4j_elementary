5a68f25177b36b61c339a2ce0081b7c90fca801c
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-12 10:21:51 +0400

    5.7. Ипотека.[#250973]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        double balance;
+        int year = 0;
+        percent = percent / 100;
+        amount = (int) (amount * percent + amount);
+        while (amount > 0) {
+            balance = amount - salary;
+            year++;
+            balance = balance * percent + balance;
+            if (balance < 0) break;
+            amount = (int) balance;
+        }
+        return year;
+    }
+}
