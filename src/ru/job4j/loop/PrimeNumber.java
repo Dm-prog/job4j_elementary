b8be1b5e662acfd8c063eb12d12048e05432ecba
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-14 02:04:39 +0400

    6.0.3. Массивы и цикл for.[#250920]

--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -1,7 +1,6 @@
 package ru.job4j.loop;
 
 public class PrimeNumber {
-
     public int calc(int finish) {
         int count = 0;
         for (int number = 2; number <= finish; number++) {
@@ -10,10 +9,5 @@ public class PrimeNumber {
             }
         }
         return count;
-    }
-
-    public static void main(String[] args) {
-        PrimeNumber p = new PrimeNumber();
-        System.out.println(p.calc(11));
     }
 }
