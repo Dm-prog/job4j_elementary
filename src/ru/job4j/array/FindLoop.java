f5510f53477ccf20ab9aaed698310ba877eef269
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-15 19:34:42 +0400

    6.5. Поиск индекса в диапазоне.[#250912]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -11,4 +11,19 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int index = start; index < finish; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FindLoop.indexOf(new int[]{5, 2, 10, 2, 4}, 2, 2, 4));
+    }
 }
