78a62528bec33aa9d09b5db6532aeee1d6e9971c
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-16 14:06:33 +0400

    6.4. Слово начинается с ...[#250907]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -3,10 +3,10 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        String s1 = new String(word);
-        String s2 = new String(pref);
-        if (!s1.startsWith(s2))
-            return false;
+        for (int i = 0; i < word.length - (word.length - pref.length); i++) {
+            if (word[i] != pref[i])
+                return false;
+        }
         return result;
     }
 }
