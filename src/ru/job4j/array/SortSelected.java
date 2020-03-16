c36ddd79e55d13acf0ec541115db5717e0a33765
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-16 22:07:25 +0400

    6.5.3. Сортировка выборкой.[#250913]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -3,9 +3,20 @@ package ru.job4j.array;
 public class SortSelected {
 
     public static int[] sort(int[] data) {
-        for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, 0, data.length);
-            int index = FindLoop.indexOf(data, min, 0, data.length);
+        int min = MinDiapason.findMin(data, 0, data.length - 1);
+        int index = FindLoop.indexOf(data, min, 0, data.length);
+        boolean isSorted = false;
+        int buf;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < data.length - 1; i++) {
+                if (data[i] > data[i + 1]) {
+                    isSorted = false;
+                    buf = data[i];
+                    data[i] = data[i + 1];
+                    data[i + 1] = buf;
+                }
+            }
         }
         return data;
     }
--- test/ru/job4j/array/SortSelectedTest.java
+++ test/ru/job4j/array/SortSelectedTest.java
@@ -5,11 +5,28 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class SortSelectedTest {
+
     @Test
     public void whenSort() {
         int[] input = new int[] {3, 4, 1, 2, 5};
         int[] result = SortSelected.sort(input);
         int[] expect = new int[] {1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortThree() {
+        int[] input = new int[] {3, 2, 5};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {2, 3, 5};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortive() {
+        int[] input = new int[] {5, 7, 1, 3, 8};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 3, 5, 7, 8};
         assertThat(result, is(expect));
     }
 }
