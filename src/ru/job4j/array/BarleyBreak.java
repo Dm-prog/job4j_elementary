fa26071ccc03cbb49c2900eaa518feab7559d4b8
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-18 00:26:09 +0400

    6.6.1. Двухмерный массив. Вывод на консоль.[#250925]

--- src/ru/job4j/array/BarleyBreak.java
+++ src/ru/job4j/array/BarleyBreak.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class BarleyBreak {
     public static void main(String[] args) {
-        int[][] nums = new int[3][3]; // объявили двухмерный массив
+        int[][] nums = new int[3][3];
         int  k = 1;
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++) {
