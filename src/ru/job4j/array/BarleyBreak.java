44adf13baefd968708a9d0ffc3a20e78b2c2ede0
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-18 00:22:45 +0400

    6.6.1. Двухмерный массив. Вывод на консоль.[#250925]

--- src/ru/job4j/array/BarleyBreak.java
+++ src/ru/job4j/array/BarleyBreak.java
@@ -2,16 +2,15 @@ package ru.job4j.array;
 
 public class BarleyBreak {
     public static void main(String[] args) {
-        int[][] mas = new int[3][3];
-        mas[0][0] = 1;
-        mas[0][1] = 2;
-        mas[0][2] = 3;
-        mas[1][0] = 4;
-        mas[1][1] = 5;
-        mas[1][2] = 6;
-        mas[2][0] = 7;
-        mas[2][1] = 8;
-        mas[2][2] = 9;
+        int[][] nums = new int[3][3]; // объявили двухмерный массив
+        int  k = 1;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                nums[i][j] = k++;
+            }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++)
+                System.out.println(nums[i][j] + " ");
+        }
     }
-
 }
--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class SortSelected {
 
     public static int[] sort(int[] data) {
-        int min = MinDiapason.findMin(data, 0, data.length - 1);
+        int min = MinDiapason.findMin(data, 0, data.length);
         int index = FindLoop.indexOf(data, min, 0, data.length);
         boolean isSorted = false;
         int buf;
