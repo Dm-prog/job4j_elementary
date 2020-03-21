11a3453d98468d74f26112dc09ed57c00280f503
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-21 17:57:16 +0400

    6.5.3. Сортировка выборкой.[#250913]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -1,18 +1,25 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class SortSelected {
     public static int[] sort(int[] data) {
-        int min = MinDiapason.findMin(data, 0, data.length);
-        int index = FindLoop.indexOf(data, min, 0, data.length);
-        for (int i = 1; i < data.length - 1; i++) {
-            if (data[i] < data[i - 1]) {
-                FindLoop.indexOf(data, min, i, i - 1);
-            }
+        int min = MinDiapason.findMin(data, 0, data.length - 1);
+        int index = FindLoop.indexOf(data, min, 0, data.length - 1);
+        int x = 0;
+        for (int i = 0; i < data.length; i++) {
+            MinDiapason.findMin(data, 0, data.length - 1);
+            FindLoop.indexOf(data, min, 0, data.length - 1);
+            x = data[i];
+            data[i] = data[index];
+            data[index] = x;
         }
         return data;
     }
 
     public static void main(String[] args) {
-        System.out.println(sort(new int[]{3, 4, 1, 2, 5}));
+        int[] arr = new int[]{3, 4, 1, 2, 5};
+        System.out.println(Arrays.toString(sort(arr)));
+
     }
 }
