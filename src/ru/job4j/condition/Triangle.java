bc85326c46c731d6e41455908af3b0bb281dde72
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-30 16:07:41 +0400

    2.2. Рефакторинг - Площадь треугольника.[#251071]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,6 +1,35 @@
 package ru.job4j.condition;
 
 public class Triangle {
+
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return p;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            double d = Math.sqrt(p *(p - a) * (p - b) * (p - c));
+            rsl = d;
+        }
+        return rsl;
+    }
+
     public static boolean exist(double ab, double ac, double bc) {
         return ab + ac > bc && ab + bc > ac && ac + bc > ab;
     }
