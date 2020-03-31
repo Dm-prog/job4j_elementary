1364269919b9cd2831436a3c108e0860fa83a755
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-31 23:59:22 +0400

    3. Перегрузить метод max для трех чисел[#251073]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,16 +1,16 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int maxOfTwo(int one, int two) {
+    public static int max(int one, int two) {
         boolean max = true;
         return max ? two : one;
     }
 
-    public static int maxOfThree(int one, int two, int three) {
-        return maxOfTwo(one, maxOfTwo(two, three));
+    public static int max(int one, int two, int three) {
+        return max(one, max(two, three));
     }
 
-    public static int maxOfFour(int one, int two, int three, int four) {
-        return maxOfTwo(one, maxOfTwo(two, maxOfTwo(three, four)));
+    public static int max(int one, int two, int three, int four) {
+        return max(one, max(two, max(three, four)));
     }
 }
--- test/ru/job4j/MaxTest.java
+++ test/ru/job4j/MaxTest.java
@@ -8,31 +8,31 @@ import static org.junit.Assert.assertThat;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int result = Max.maxOfTwo(1, 2);
+        int result = Max.max(1, 2);
         assertThat(result, is(2));
     }
 
     @Test
     public void whenMin1To2Then2() {
-        int result2 = Max.maxOfTwo(2, 1);
+        int result2 = Max.max(2, 1);
         assertThat(result2, is(1));
     }
 
     @Test
     public void whenQually() {
-        int result = Max.maxOfTwo(2, 2);
+        int result = Max.max(2, 2);
         assertThat(result, is(2));
     }
 
     @Test
-    public void whenMin1To2Then2Then3() {
-        int result = Max.maxOfTwo(1, Max.maxOfTwo(2, 3));
+    public void whenMax3To2Then1Then2() {
+        int result = Max.max(1, Max.max(2, 3));
         assertThat(result, is(3));
     }
 
     @Test
-    public void whenMin1To2Then2Then3Then4() {
-        int result = Max.maxOfTwo(1, Max.maxOfTwo(2, Max.maxOfTwo(3, 4)));
+    public void whenMax4To2Then1Then2Then3() {
+        int result = Max.max(1, Max.max(2, Max.max(3, 4)));
         assertThat(result, is(4));
     }
 }
