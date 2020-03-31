fbe94f1279a2634a27d5ad341c2b2e103cbce58d
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-03-31 11:21:21 +0400

    4. Расстояние между точками в трехмерном пространстве.[#251072]

--- test/ru/job4j/PointTest.java
+++ test/ru/job4j/PointTest.java
@@ -7,19 +7,17 @@ import ru.job4j.condition.Point;
 public class PointTest {
     @Test
     public void distance() {
-        int x1In = 0;
-        int y1In = 0;
-        int x2In = 2;
-        int y2In = 0;
+        Point point = new Point(4, 2);
         double expected = 2.0;
-        double out = Point.distance(new Point(2, 2));
+        double out = point.distance(new Point(2, 2));
         Assert.assertEquals(expected, out, 0.1);
     }
 
     @Test
-    public void distance() {
+    public void distance3d() {
+        Point point = new Point(2, 4, 2);
         double expected = 2.0;
-        double out = distance(new Point(2, 2, 3));
+        double out = point.distance3d(new Point(2, 2, 2));
         Assert.assertEquals(expected, out, 0.1);
     }
 }
