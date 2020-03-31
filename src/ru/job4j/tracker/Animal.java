5cac8a26b835add193320f7a4b0b6ab4426e59ad
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-04-01 00:59:51 +0400

    1. Конструктор по умолчанию.[#251085]

---
+++ src/ru/job4j/tracker/Animal.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+public class Animal {
+    public Animal() {
+        System.out.println("Animal");
+    }
+
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger();
+    }
+}
