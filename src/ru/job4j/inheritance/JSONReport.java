820db33d9a5815eecace0370ca2ecbc28f3ddb05
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-04-01 14:10:36 +0400

    5. Аннотация @Override[#251088]

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -1,6 +1,7 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         TextReport report = new TextReport();
         String text = report.generate("name : name", "body : body");
