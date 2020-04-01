25474604f5ab61e6b5039c9340bd6fe480c2b6d9
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-04-01 13:05:26 +0400

    4. Переопределение[#251087]

---
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        TextReport report = new TextReport();
+        String text = report.generate(name + " : name", body + " : body");
+        return text;
+    }
+}
