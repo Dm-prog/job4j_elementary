c94eebebfb4766d0acdd26e4556ab0b616c161d0
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-04-01 22:58:05 +0400

    3. Удаление моделей из массива.[#251095]

---
+++ src/ru/job4j/pojo/Shop.java
@@ -0,0 +1,57 @@
+package ru.job4j.pojo;
+
+public class Shop {
+    public Product[] delete(Product[] products, int index) {
+        for (int i = 0; i < products.length; i++) {
+            products[index] = products[index + 1];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+
+    public static void main(String[] args) {
+        Shop shop = new Shop();
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+
+        System.out.println(shop.delete(new Product[] {products[0],
+                products[1], null, products[2], null}, 3));
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        products[1] = null;
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
+        products[1] = products[2];
+        products[2] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+}
