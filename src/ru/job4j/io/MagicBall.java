767681a309d1c6f61a62f04a7d8e98faded25eb5
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-04-05 16:42:19 +0400

    1. Чтение из консоли. Класс Scanner.[#251034]

--- src/ru/job4j/io/MagicBall.java
+++ src/ru/job4j/io/MagicBall.java
@@ -7,15 +7,13 @@ public class MagicBall {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        sc.nextLine();
         int answer = new Random().nextInt(3);
         if (answer == 0) {
-            sc.nextLine();
             System.out.println("Да");
         } else if (answer == 1) {
-            sc.nextLine();
             System.out.println("Нет");
         } else {
-            sc.nextLine();
             System.out.println("Может быть");
         }
     }
