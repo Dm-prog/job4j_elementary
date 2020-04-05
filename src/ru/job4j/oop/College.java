dfa094c7576ff09a932c74b87b70654c45d4099e
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-04-05 16:17:47 +0400

    3.1. Приведение типов. Повышение типа и понижение типа.[#251039]

--- src/ru/job4j/oop/College.java
+++ src/ru/job4j/oop/College.java
@@ -2,10 +2,8 @@ package ru.job4j.oop;
 
 public class College {
     public static void main(String[] args) {
-        Student s = new Freshman();
-        Freshman freshFromStudent = (Freshman) s;
-
-        Object object = new Freshman();
-        Freshman freshFromObject = (Freshman) object;
+        Freshman freshman = new Freshman();
+        Student st = freshman;
+        Object object = freshman;
     }
 }
