8c1bcc4d726bee50f68c403e7ae311679b3775de
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-04-08 13:41:34 +0400

    2.1. Реализация класса StartUI[#251045]

--- src/ru/job4j/tracker/StartUI.java
+++ src/ru/job4j/tracker/StartUI.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class StartUI {
 
     public void init(Scanner scanner, Tracker tracker) {
-        boolean run = false;
+        boolean run = true;
         while (run) {
             this.showMenu();
             System.out.print("Select: ");
@@ -17,12 +17,30 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
             } else if (select == 1) {
-
+                System.out.println("=== Show all items ====");
+                System.out.print("Enter items: ");
+                tracker.findAll();
             } else if (select == 2) {
+                System.out.println("=== Edit item ====");
+                System.out.print("Enter id: ");
+                String id = scanner.nextLine();
+                Item item = new Item(id);
+                tracker.replace(id, item);
             } else if (select == 3) {
+                System.out.println("=== Create Delete item ====");
+                System.out.print("Enter item: ");
+                String name = scanner.nextLine();
+                tracker.delete(name);
             } else if (select == 4) {
+                System.out.println("=== Find item by Id ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                tracker.delete(name);
             } else if (select == 5) {
-
+                System.out.println("=== Find items by name ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                tracker.findByName(name);
             } else if (select == 6) {
                 run = false;
             }
@@ -31,7 +49,11 @@ public class StartUI {
 
     private void showMenu() {
         System.out.println("Menu.");
-        // добавить остальные пункты меню.
+        System.out.println("Show all items");
+        System.out.println("Edit item");
+        System.out.println("Delete item");
+        System.out.println("Find item by Id");
+        System.out.println("Find items by name");
     }
 
 
