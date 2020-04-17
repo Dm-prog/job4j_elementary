f0a2ae9d3a0635f20a10675a7b928c10b67a0881
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-04-17 17:12:01 +0400

    0.4. Пользовательские исключения.[#251055]

--- src/ru/job4j/ex/FindEl.java
+++ src/ru/job4j/ex/FindEl.java
@@ -11,13 +11,14 @@ public class FindEl {
 
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (String st : value) {
-            if (st.equals(key)) {
-                System.out.println(st);
-                rsl++;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                System.out.println(key);
+                rsl = index;
                 break;
             }
         }
+
         if (rsl == -1) {
             throw new ElementNotFoundException();
         }
