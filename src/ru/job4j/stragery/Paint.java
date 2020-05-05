bab41aaecc56111a4f07bdc7ca5067f0521ec92f
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-05-05 17:12:49 +0400

    7. Шаблон проектирования - Стратегия.[#251047]

---
+++ src/ru/job4j/stragery/Paint.java
@@ -0,0 +1,15 @@
+package ru.job4j.stragery;
+
+public class Paint {
+    public static void main(String[] args) {
+        Paint paint = new Paint();
+        Triangle triangle = new Triangle();
+        Square square = new Square();
+        paint.draw(triangle);
+        paint.draw(square);
+    }
+
+    public void draw(Shape shape) {
+        System.out.println(shape.draw());
+    }
+}
