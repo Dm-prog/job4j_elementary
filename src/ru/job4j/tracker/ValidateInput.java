d1440a401510c70c735d050fbfc2c59bc4bec1e7
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-05-07 13:32:31 +0400

    1. Обеспечить бесперебойную работу приложения Tracker.[#251059]

--- src/ru/job4j/tracker/ValidateInput.java
+++ src/ru/job4j/tracker/ValidateInput.java
@@ -1,7 +1,7 @@
 
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput implements Input {
+public class ValidateInput extends ConsoleInput {
 
     @Override
     public int askInt(String question) {
