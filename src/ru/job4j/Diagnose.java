f37e10b41fece6ae18b44464702fdf055a7dc556
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-05-09 12:51:47 +0400

    A

--- src/ru/job4j/Diagnose.java
+++ src/ru/job4j/Diagnose.java
@@ -1,4 +1,4 @@
-package ru.job4j.tracker;
+package ru.job4j;
 
 public class Diagnose extends Doctor {
     public void coronavirus() {
--- src/ru/job4j/Programmer.java
+++ src/ru/job4j/Programmer.java
@@ -1,4 +1,6 @@
-package ru.job4j.tracker;
+package ru.job4j;
+
+import ru.job4j.Engineer;
 
 public class Programmer extends Engineer {
     String name;
