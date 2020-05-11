fdf40ac9405ff16065dad8bb9c282d113bb4efc4
Dm-prog <53370362+Dm-prog@users.noreply.github.com>
2020-05-11 13:04:23 +0400

    Сдача в кофе машине. [#34741]

--- src/ru/job4j/sort/Machine.java
+++ src/ru/job4j/sort/Machine.java
@@ -3,21 +3,17 @@ package ru.job4j.sort;
 import java.util.Arrays;
 
 public class Machine {
-
     private final int[] COINS = {10, 5, 2, 1};
-
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int diff = money - price;
-        while (diff > 0) {
-            for (int i = diff; diff > 0; diff--) {
-                for (int index = 0; index < COINS.length; index++) {
-                    if (COINS[index] <= diff) {
-                        rsl[size] = COINS[index];
-                        diff = diff - COINS[index];
-                        size++;
-                    }
+        for (int index = 0; index < COINS.length; index++) {
+            while (diff - COINS[index] >= 0) {
+                if (COINS[index] <= diff) {
+                    rsl[size] = COINS[index];
+                    diff = diff - COINS[index];
+                    size++;
                 }
             }
         }
