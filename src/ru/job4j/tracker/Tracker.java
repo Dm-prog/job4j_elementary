3242a8389133f7a3b8cf3f8abca1a6624a6381f7
Dmitriy K <dmitriyk80@bk.ru>
2020-05-27 20:23:41 +0400

    6. Изменить программу Tracker из 2-го модуля[10039#251009]

--- src/ru/job4j/tracker/Tracker.java
+++ src/ru/job4j/tracker/Tracker.java
@@ -4,50 +4,26 @@ import java.util.*;
 
 public class Tracker {
 
-    //private final Item[] items = new Item[100];
     private final List<Item> items = new ArrayList<>();
-
-    private int position = 0;
 
     public Item add(Item item) {
         item.setId(generateId());
-        this.items.set(position++, item);
+        this.items.add(item);
         return item;
     }
-
-//    public Item[] findAll() {
-//        return Arrays.copyOf(items, position);
-//    }
 
     public List<Item> findAll() {
         return items;
     }
 
-//    public Item[] findByName(String key) {
-//        int size = 0;
-//        Item[] result = new Item[position];
-//        for (int index = 0; index < position; index++) {
-//            Item item = items[index];
-//            if (item.getName().equals(key)) {
-//                result[size] = item;
-//                size++;
-//            }
-//        }
-//        Arrays.copyOf(result, size);
-//        return result;
-//    }
-
     public List<Item> findByName(String key) {
-        int size = 0;
         List<Item> result = new ArrayList<>();
-        for (int index = 0; index < position; index++) {
+        for (int index = 0; index < items.size(); index++) {
             Item item = items.get(index);
             if (item.getName().equals(key)) {
                 result.add(item);
-                size++;
             }
         }
-        Collections.copy(result, items);
         return result;
     }
 
@@ -63,7 +39,7 @@ public class Tracker {
 
     private int indexOf(String id) {
         int rsl = -1;
-        for (int index = 0; index < position; index++) {
+        for (int index = 0; index < items.size(); index++) {
             if (items.get(index).getId().equals(id)) {
                 rsl = index;
                 break;
@@ -77,7 +53,7 @@ public class Tracker {
         int index = indexOf(id);
         if (index != -1) {
             item.setId(id);
-            items.add(item);
+            items.set(index, item);
             result = true;
         }
         return result;
@@ -86,12 +62,7 @@ public class Tracker {
     public boolean delete(String id) {
         int index = indexOf(id);
         if (index != -1) {
-            int start = index + 1;
-            int distPos = index;
-            int size = position - index;
-            items.add(null);
-            position--;
-            System.arraycopy(items, start, items, distPos, size);
+            items.remove(index);
         }
         return true;
     }
