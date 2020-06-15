d62a313c57ef1f403c3abdd47ce08a292aa5351a
Dmitriy K <dmitriyk80@bk.ru>
2020-06-15 12:32:06 +0400

    0. Сортировка[10096#251002]

--- test/ru/job4j/tracker/SortByItemTest.java
+++ test/ru/job4j/tracker/SortByItemTest.java
@@ -2,8 +2,8 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -12,19 +12,25 @@ public class SortByItemTest {
     @Test
     public void sortByItemNameReverce() {
         SortByItemReverce sortByItemReverce = new SortByItemReverce();
-        Set<Item> items = new TreeSet<>(sortByItemReverce);
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Victor"));
         items.add(new Item("Anna"));
         items.add(new Item("Ivan"));
-        int result = sortByItemReverce.compare(new Item("Ivan"), new Item("Anna"));
-        assertThat(result, is(sortByItemReverce.compare(new Item("Ivan"), new Item("Anna"))));
+        Collections.sort(items, sortByItemReverce.compare(items.get(0), items.get(1)));
+        List<Item> expectedItems = new ArrayList<>();
+        expectedItems.add(new Item("Anna"));
+        expectedItems.add(new Item("Ivan"));
+        expectedItems.add(new Item("Victor"));
+        assertThat(expectedItems, is(items));
     }
 
     @Test
     public void sortByItemName() {
         SortByItem sortByItem = new SortByItem();
-        Set<Item> items = new TreeSet<>(sortByItem);
+        List<Item> items = new ArrayList<>();
         items.add(new Item("Anna"));
         items.add(new Item("Ivan"));
+        Collections.sort(items, sortByItem);
         int result = sortByItem.compare(new Item("Anna"), new Item("Ivan"));
         assertThat(result, is(sortByItem.compare(new Item("Anna"), new Item("Ivan"))));
     }
