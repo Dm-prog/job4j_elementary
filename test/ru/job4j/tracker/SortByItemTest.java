d117131c0274c21f9550bb51e98299bacfcc0ad6
Dmitriy K <dmitriyk80@bk.ru>
2020-06-16 16:31:21 +0400

    0. Сортировка[10096#251002]

--- test/ru/job4j/tracker/SortByItemTest.java
+++ test/ru/job4j/tracker/SortByItemTest.java
@@ -2,37 +2,41 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 
 public class SortByItemTest {
 
     @Test
-    public void sortByItemNameReverce() {
-        SortByItemReverce sortByItemReverce = new SortByItemReverce();
+    public void sortByItemName() {
         List<Item> items = new ArrayList<>();
-        items.add(new Item("Victor"));
         items.add(new Item("Anna"));
+        items.add(new Item("Victor"));
         items.add(new Item("Ivan"));
-        Collections.sort(items, sortByItemReverce.compare(items.get(0), items.get(1)));
+        Collections.sort(items, new SortByItem());
         List<Item> expectedItems = new ArrayList<>();
         expectedItems.add(new Item("Anna"));
         expectedItems.add(new Item("Ivan"));
         expectedItems.add(new Item("Victor"));
-        assertThat(expectedItems, is(items));
+        assertEquals(expectedItems, is(items));
     }
 
     @Test
-    public void sortByItemName() {
-        SortByItem sortByItem = new SortByItem();
+    public void sortByItemNameReverce() {
         List<Item> items = new ArrayList<>();
-        items.add(new Item("Anna"));
         items.add(new Item("Ivan"));
-        Collections.sort(items, sortByItem);
-        int result = sortByItem.compare(new Item("Anna"), new Item("Ivan"));
-        assertThat(result, is(sortByItem.compare(new Item("Anna"), new Item("Ivan"))));
+        items.add(new Item("Anna"));
+        items.add(new Item("Victor"));
+        Collections.sort(items, new SortByItemReverce());
+        List<Item> expectedItems = new ArrayList<>();
+        expectedItems.add(new Item("Victor"));
+        expectedItems.add(new Item("Ivan"));
+        expectedItems.add(new Item("Anna"));
+        assertEquals(expectedItems, is(items));
     }
 
 }
