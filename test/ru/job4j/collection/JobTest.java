ddb6aeb871a58a2f9c8095cd281374b1e8b37020
Dmitriy K <dmitriyk80@bk.ru>
2020-06-27 13:44:29 +0400

    2. Комбинированный компаратор.[10036#251001]

--- test/ru/job4j/collection/JobTest.java
+++ test/ru/job4j/collection/JobTest.java
@@ -2,7 +2,6 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 import java.util.Comparator;
-
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
