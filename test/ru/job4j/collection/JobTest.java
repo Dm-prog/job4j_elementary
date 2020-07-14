c24a6c760c58c20756a1daefdc9a8294fce94793
Dmitriy K <dmitriyk80@bk.ru>
2020-07-14 14:04:19 +0400

    3. Компаратор для строк[35008#250998]

--- test/ru/job4j/collection/JobTest.java
+++ test/ru/job4j/collection/JobTest.java
@@ -2,19 +2,29 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 import java.util.Comparator;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.lessThan;
+
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class JobTest {
     @Test
-    public void whenCompatorByNameAndPrority() {
+    public void whenCompatorByNameAndPrority1() {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
                 new Job("Fix bug", 1)
         );
         assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenCompatorByNameAndPrority2() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Impl task", 1)
+        );
+        assertThat(rsl, greaterThan(0));
     }
 
     @Test
