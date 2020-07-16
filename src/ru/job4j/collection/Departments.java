80637fd44618e90d620ac2ba7841f03ef58156a7
Dmitriy K <dmitriyk80@bk.ru>
2020-07-16 12:39:11 +0400

    Отсортировать департаменты[34352#250995]

--- src/ru/job4j/collection/Departments.java
+++ src/ru/job4j/collection/Departments.java
@@ -2,53 +2,44 @@ package ru.job4j.collection;
 
 import java.util.*;
 
-public class Departments implements Comparable<Departments> {
+public class Departments {
 
     public static List<String> fillGaps(List<String> deps) {
         HashSet<String> tmp = new HashSet<>();
         for (String value : deps) {
             String start = "";
-            for (String el : value.split("/")) {
+            String[] dep = value.split("/");
+            for (String el : dep) {
                 tmp.add(start + "/" + el);
             }
+
         }
         return new ArrayList<>(tmp);
     }
 
     public static void sortAsc(List<String> orgs) {
-        orgs.add("K1");
-        orgs.add("K1/SK1");
-        orgs.add("K1/SK1/SSK1");
-        orgs.add("K1/SK1/SSK2");
-        orgs.add("K1/SK2");
-        orgs.add("K2");
-        orgs.add("K2/SK1");
-        orgs.add("K2/SK1/SSK1");
-        orgs.add("K2/SK1/SSK2");
+//        orgs.add("K1");
+//        orgs.add("K1/SK1");
+//        orgs.add("K1/SK1/SSK1");
+//        orgs.add("K1/SK1/SSK2");
+//        orgs.add("K1/SK2");
+//        orgs.add("K2");
+//        orgs.add("K2/SK1");
+//        orgs.add("K2/SK1/SSK1");
+//        orgs.add("K2/SK1/SSK2");
         Collections.sort(orgs);
     }
 
     public static void sortDesc(List<String> orgs) {
-        orgs.add("K2");
-        orgs.add("K2/SK1");
-        orgs.add("K2/SK1/SSK1");
-        orgs.add("K2/SK1/SSK2");
-        orgs.add("K1");
-        orgs.add("K1/SK1");
-        orgs.add("K1/SK1/SSK1");
-        orgs.add("K1/SK1/SSK2");
-        orgs.add("K1/SK2");
-
-        DepDescComp depDescComp = new DepDescComp();
-        int result;
-        result = depDescComp.compare(orgs.get(1), orgs.get(0));
-        if (result == 0) {
-            depDescComp.compare(orgs.get(0), orgs.get(1));
-        }
-    }
-
-    @Override
-    public int compareTo(Departments o) {
-        return 1;
+//        orgs.add("K2");
+//        orgs.add("K2/SK1");
+//        orgs.add("K2/SK1/SSK1");
+//        orgs.add("K2/SK1/SSK2");
+//        orgs.add("K1");
+//        orgs.add("K1/SK1");
+//        orgs.add("K1/SK1/SSK1");
+//        orgs.add("K1/SK1/SSK2");
+//        orgs.add("K1/SK2");
+        Collections.sort(orgs, new DepDescComp());
     }
 }
