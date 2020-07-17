f9bd6a4a3fbb8042118cf901be442cbbd5a5c315
Dmitriy K <dmitriyk80@bk.ru>
2020-07-17 09:13:28 +0400

    Отсортировать департаменты[34352#250995]

--- src/ru/job4j/collection/Departments.java
+++ src/ru/job4j/collection/Departments.java
@@ -5,41 +5,25 @@ import java.util.*;
 public class Departments {
 
     public static List<String> fillGaps(List<String> deps) {
+
         HashSet<String> tmp = new HashSet<>();
         for (String value : deps) {
             String start = "";
             String[] dep = value.split("/");
             for (String el : dep) {
-                tmp.add(start + "/" + el);
+                start = start + el;
+                tmp.add(start);
+                start = start + "/";
             }
-
         }
         return new ArrayList<>(tmp);
     }
 
     public static void sortAsc(List<String> orgs) {
-//        orgs.add("K1");
-//        orgs.add("K1/SK1");
-//        orgs.add("K1/SK1/SSK1");
-//        orgs.add("K1/SK1/SSK2");
-//        orgs.add("K1/SK2");
-//        orgs.add("K2");
-//        orgs.add("K2/SK1");
-//        orgs.add("K2/SK1/SSK1");
-//        orgs.add("K2/SK1/SSK2");
         Collections.sort(orgs);
     }
 
     public static void sortDesc(List<String> orgs) {
-//        orgs.add("K2");
-//        orgs.add("K2/SK1");
-//        orgs.add("K2/SK1/SSK1");
-//        orgs.add("K2/SK1/SSK2");
-//        orgs.add("K1");
-//        orgs.add("K1/SK1");
-//        orgs.add("K1/SK1/SSK1");
-//        orgs.add("K1/SK1/SSK2");
-//        orgs.add("K1/SK2");
         Collections.sort(orgs, new DepDescComp());
     }
 }
