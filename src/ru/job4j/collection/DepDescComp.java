b3fda38b0ac8ff84d9ff11d25a5af9e61df41cdf
Dmitriy K <dmitriyk80@bk.ru>
2020-07-18 15:34:05 +0400

    Отсортировать департаменты[34352#250995]

--- src/ru/job4j/collection/DepDescComp.java
+++ src/ru/job4j/collection/DepDescComp.java
@@ -5,6 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        return o1.compareTo(o2);
+        if (o1.charAt(0) == o2.charAt(0)) {
+            return o1.compareTo(o2);
+        }
+        return 0;
     }
 }
