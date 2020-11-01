0294083b9a79d7bad39fd065bea2996641dbed26
Dm-prog <prostokota@list.ru>
2020-11-01 13:25:03 +0400

    Local-Variable Type Inference[70626#250884]

--- src/ru/job4j/search/PhoneDictionary.java
+++ src/ru/job4j/search/PhoneDictionary.java
@@ -11,7 +11,7 @@ public class PhoneDictionary {
 
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        for (Person p : persons) {
+        for (var p : persons) {
             if (p.getName().contains(key) || p.getSurname().contains(key) ||
                     p.getPhone().contains(key) || p.getAddress().contains(key)) {
                 result.add(p);
