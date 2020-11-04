adc7c884978457d54b2f7246de32856151e82560
Dm-prog <prostokota@list.ru>
2020-11-04 13:27:57 +0400

    3. Optional в банковских переводах.[214711#250885]

--- test/ru/job4j/bank/BankServiceTest.java
+++ test/ru/job4j/bank/BankServiceTest.java
@@ -11,9 +11,9 @@ public class BankServiceTest {
 
     @Test
     public void addUser() {
-        Optional<User> user = Optional.of(new User("3434", "Petr Arsentev"));
+        User user = new User("3434", "Petr Arsentev");
         BankService bank = new BankService();
-        bank.addUser(user.get());
+        bank.addUser(user);
         assertThat(bank.findByPassport("3434"), is(user));
     }
 
